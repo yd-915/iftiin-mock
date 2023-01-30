5ed89c11925f8fb93af26094774ec4da68bf6c2d
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-01-29 21:57:46 -0500

    Update Tips.js

--- mocktail-dashboard/src/containers/DashboardTabs/Tips.js
+++ mocktail-dashboard/src/containers/DashboardTabs/Tips.js
@@ -1,6 +1,6 @@
 const Tips = [
   'Response Body on the left is the response you wish to obtain.',
-  'Mocktail is a free, containerized open-source mock server.',
+  'Iftiin-Mock from Iftiin-HUb is a free, containerized open-source mock server.',
   'Please open tickets if you encounter any bugs on github.com/Huseyinnurbaki/mocktail',
   'You can email me if you have any questions, feedbacks or suggetions. huseyin.nurbaki@gmail.com ',
   'Original docker image is hhaluk/mocktail',
