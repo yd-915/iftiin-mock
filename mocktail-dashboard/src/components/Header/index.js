7c21d0bac3c659d0476892109730d882122f401b
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-08 14:10:22 -0500

    Update index.js

--- mocktail-dashboard/src/components/Header/index.js
+++ mocktail-dashboard/src/components/Header/index.js
@@ -10,7 +10,7 @@ const Header = () => {
             src="./mock.png"
             className="headerimg"
             alt=""
-            style={{ height: '100px', alignSelf: 'center',  justify-content: 'left';display: 'inline-block';}}
+            style={{ height: '100px', alignSelf: 'center',  justifyContent: 'left'; display: 'inline-block';}}
           />
         </Col>
       </Row>
