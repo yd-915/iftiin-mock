5d44fbc407e15695df50a17dd450d27f6f94b1c8
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-08 23:08:30 -0500

    Update index.js

--- mocktail-dashboard/src/components/Header/index.js
+++ mocktail-dashboard/src/components/Header/index.js
@@ -6,13 +6,13 @@ const Header = () => {
     <Container>
       <Row className="justify-content-md-center">
         <Col xs={12} sm={4} md={4}>
-    <a href="https://www.iftiinhub.pro/">
+    
           <img
             src="./return.png"
             className="headerimg"
             alt=""
           />
-              </a>
+     
         </Col>
       </Row>
     </Container>
