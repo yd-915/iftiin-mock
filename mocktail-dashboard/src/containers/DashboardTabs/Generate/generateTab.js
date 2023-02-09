2499fc3dbe5e5df71c9e83459588be23f7005db8
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-09 14:24:12 -0500

    Update generateTab.js

--- mocktail-dashboard/src/containers/DashboardTabs/Generate/generateTab.js
+++ mocktail-dashboard/src/containers/DashboardTabs/Generate/generateTab.js
@@ -84,7 +84,7 @@ function GenerateTab(props) {
             disabled={false}
             style={{ marginLeft: '20px' }}
             variant="warning"
-            onClick={(event)·=>·(window.location.href·=·'https://www.iftiinhub.pro/homepage')}>
+            onClick={(event) => (window.location.href·=·'https://www.iftiinhub.pro/homepage')}>
             Home
           </Button>
         </Col>
