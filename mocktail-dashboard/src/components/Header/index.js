031b34c7ae9ca1f86808c41a3fa5e41c439dafad
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-09 12:44:09 -0500

    Update index.js

--- mocktail-dashboard/src/components/Header/index.js
+++ mocktail-dashboard/src/components/Header/index.js
@@ -16,19 +16,6 @@ const Header = () => {
         </Col>
       </Row>
     </Container>
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col xs={12} sm={4} md={4}>
-          <img
-            src="./header.webp"
-            className="headerimg"
-            alt=""
-            style={{ height: '100px', alignSelf: 'center' }}
-          />
-        </Col>
-      </Row>
-    </Container>
-</>
   );
 };
 
